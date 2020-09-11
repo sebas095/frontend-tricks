8ec9401ce7e79f557863cc8a6d9809ace7b78723
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-09-11 02:24:43 -0500

    ADD - basic menu responsive added

---
+++ responsive-menu/js/menu.js
@@ -0,0 +1,6 @@
+const iconMenu = document.getElementById("icon-menu");
+const mainMenu = document.getElementById("main-menu");
+
+iconMenu.addEventListener("click", () =>
+  mainMenu.classList.toggle("menu--show")
+);
